ce8a04b8e6900219b99df6fb6f2da379274505ca
ParthB <41703901+ParthBasole@users.noreply.github.com>
2021-02-24 22:26:27 +0530

    Added Comments for better readability
    ....there might be few spelling mistakes tho

--- JUNITTEST/src/jUNITTEST/JunitTest.java
+++ JUNITTEST/src/jUNITTEST/JunitTest.java
@@ -18,31 +18,31 @@ public class JunitTest {
 		}
 		
 		@Test
-		public void testAdd() throws NegativeEx {
+		public void testAdd() throws NegativeEx {    //NULL Test
 	     assertEquals(0,calc.Add(""),"Null String should give out 0");
-}
+        }
 		
 		@Test
-		public void testAddS() throws NegativeEx {
+		public void testAddS() throws NegativeEx { //Simple addition
 	     assertEquals(5,calc.Add("2,3"),"This String should give out 5");
 }
-		@Test
-		public void testAddMult() throws NegativeEx{
+		@Test 
+		public void testAddMult() throws NegativeEx{ //Multiple addition
 	     assertEquals(24,calc.Add("2,3,6,7,6"),"This String should give out 5");
 
 		}
 		@Test
-		public void testDiffDel() throws NegativeEx
+		public void testDiffDel() throws NegativeEx  //Uses different Delimiter which is \n with ,
 		{
 			assertEquals(7,calc.Add("2\n2,3"),"This should give out 6 check split");
 		}
 		@Test
-		public void testCustDel() throws NegativeEx
+		public void testCustDel() throws NegativeEx    //Uses custom delimiter
 		{
 			assertEquals(7,calc.Add("//;\n2;3;2"),"This should give out 7 check split");
 		}
 		@Test
-		public void testCustDel1() throws NegativeEx
+		public void testCustDel1() throws NegativeEx  //another one of custom delimiter 
 		{
 			assertEquals(9,calc.Add("//p\n2p3p2p2"),"This should give out 9 check split");
 		}
@@ -50,7 +50,7 @@ public class JunitTest {
 		//@SuppressWarnings("deprecation")
 		
 		@Test
-		public void testNegatov()
+		public void testNegatov() //exception handling when negative
 		{
 			NegativeEx thrown=assertThrows(
 					NegativeEx.class,
@@ -58,43 +58,38 @@ public class JunitTest {
 					"expected negative throw"
 					);
 		}
-		@Test
-		public void checkMethodCalls() throws NegativeEx
-		{
-			System.out.print(calc.GetCalledCount());
-		}
 		
 		@Test
-		public void moreThanTwothousand() throws NegativeEx
-		{
+		public void moreThanTwothousand() throws NegativeEx //more than thousand not accepted
+ 		{
 			assertEquals(326,calc.Add("4,322,3000"));
 		}
 		
 		@Test
-		public void anyLength() throws NegativeEx
+		public void anyLength() throws NegativeEx  //string delimiter
 		{
 			assertEquals(626,calc.Add("//+++**\n4+++**322+++**300"));
 		}
 		
 		@Test
-		public void FuncChecker()
-		{
+		public void FuncChecker() //helper function checker the one which extracts numbers 
+		{ 
 			calc.stringSplit("+++*","4+++*322+++*3000");
 		}
 		
 		@Test
-		public void MultipleDel() throws NegativeEx
+		public void MultipleDel() throws NegativeEx   //Multiple delimiter checker
 		{
 			assertEquals(100,calc.Add("//[%][*]\n4%56*40"));
 		}
 		
 		@Test
-		public void Brack() throws NegativeEx
+		public void Brack() throws NegativeEx //another function checker this one deals with box cutter one
 		{
 			calc.indexofallBrack("[%][*]","4%56*40");
 		}
 		@Test
-		public void MultipleLDel() throws NegativeEx
+		public void MultipleLDel() throws NegativeEx   //Uses multiple string delimeters
 		{
 			assertEquals(100,calc.Add("//[%**][##*]\n4%**56##*40"));
 		}
--- JUNITTEST/src/jUNITTEST/StringCalculator.java
+++ JUNITTEST/src/jUNITTEST/StringCalculator.java
@@ -3,92 +3,89 @@ package jUNITTEST;
 import java.util.ArrayList;
 
 public class StringCalculator {
-public int a=0;
 public int flag=0;
-	public int Add(String numbers) throws NegativeEx
-		{ a++;
-		if(numbers.length()==0)
-		return 0;
-		int sum=0;
-		String[] strarr = null;
-		String[] rstrarr = null;
-		//System.out.print(numbers.substring(0,2));	
-		if(numbers.substring(0,2).equals("//"))
-    		{
-	        strarr=numbers.split("\n",2);
-	      //  System.out.print(strarr[0].length());
-					if(strarr.length>1){
-						String del=strarr[0].substring(strarr[0].indexOf("/") + 2, strarr[0].length());
+	public int Add(String numbers) throws NegativeEx   //Adds String
+		{ 
+			if(numbers.length()==0)
+			return 0;
+			int sum=0;
+			String[] strarr = null;
+			String[] rstrarr = null;
+			//System.out.print(numbers.substring(0,2));	
+				if(numbers.substring(0,2).equals("//"))  //Checks if first two characters are "//"  
+					{	strarr=numbers.split("\n",2);    // Splits the String in two different parts keeping \n as its pivot point 
+		     
+							if(strarr.length>1) //Condition added to see if actually number is splittable or not.
+							 {    
+								String del=strarr[0].substring(strarr[0].indexOf("/") + 2, strarr[0].length()); //if custom delimiter is given this extracts portion between //  and \n 
 					
-						String Target=indexofallBrack(del,strarr[1]);
-						if(flag==1)
+						String Target=indexofallBrack(del,strarr[1]);  //if delimiter has multiple delimiter 
+						                                               //i.e "//[delimeter1][[delimeter2]\n" 
+																	   //then this helper function takes those delimiters and replaces them with "," in the the addition part of string 
+						
+						 										
+						if(flag==1)                                //This flag is set in helper function below which checks if there are multiple delimiter or not if there are then flag =1 else 0
 							{rstrarr=stringSplit(",",Target);}
 						else
-						{rstrarr=stringSplit(del,Target);}
-					}
-    		}
-		else{
-			rstrarr=numbers.split("\n|\\,",-1);	
+						{rstrarr=stringSplit(del,Target);} //rstrarr is a string array which has all the number present as string
+                 					}
+    		        }//Closes if there is custom delimiter 
+			else{ //if default case is present 
+			rstrarr=numbers.split("\n|\\,",-1); //or regex used for checking if the string has \n or , as delimiter	
 	 	    }
-		    for(String a: rstrarr)
-				  {   int temp=Integer.parseInt(a);
-			        if(temp<0)
+		    for(String a: rstrarr)     // Iterator to take single number in consideration
+				  {   int temp=Integer.parseInt(a); //parsing the string as int
+			        if(temp<0)                       //if negative number is present then throws custom made exception check the other file attached for defination
 			        	{
 			        		throw new NegativeEx(a);
 			        	}
-			        if(temp<1000)
+			        if(temp<1000)     // adds number only if they are below 1000 
 			        sum+=temp; 
 					}
 		    return sum;
 			 }
 	
-	public int GetCalledCount()
-	{
-		return a;
-	}
 	
 	
-	
-	
-	
-	
-	
-	public String[] stringSplit(String str,String target)
+	public String[] stringSplit(String str,String target) //Helper function which splits string via string i.e abra ##$$ cadabra becomes a array[abra,cadabra]  
  	{  //Object arr;
 		 String[] rtr;
-		 if(str.length()==1)
+		 if(str.length()==1)              //if the given delimiter has single character as its defination then this uses System Split method
 		 {
 			 rtr=target.split(str,-1);
 			 return rtr;
 		 }
-		ArrayList<String> ar = new ArrayList<String>();
-        int F_index=0,B_index=0,W_index=0;
-        B_index=str.length();
+		ArrayList<String> ar = new ArrayList<String>();//String aray list generation for dynamic addition
+        int F_index=0,B_index=0,W_index=0;  //F_index =Front Index B_index= Back_Index  
+        B_index=str.length(); 
  	    String temp;
-        while(B_index<=target.length())
+        while(B_index<=target.length())  // logic behind this is basically check if target has str i.e del in main code
+        								//present in it and then break target string to make a array of string numbers which are seperated by str
         	{
-        	temp=target.substring(F_index,B_index);
-        //	System.out.println(temp+"  B_index "+B_index+"  F_index "+F_index+"  W_index "+W_index); 
-        	if(temp.equals(str))
-        	 	{ar.add(target.substring(W_index,F_index));
-        	 //	 System.out.println("Added"+target.substring(W_index,F_index));
+        	temp=target.substring(F_index,B_index); //makes substring     
+   
+        	if(temp.equals(str))                    //checks if it is equal to str
+        	 	{ar.add(target.substring(W_index,F_index));   //if it then adds it to our collection of numerical string
+ 
  		         F_index=B_index;
  		         W_index=B_index;
- 		         B_index+=Math.min(str.length(),target.length()-B_index);
- 		       //  System.out.println(target.length()-B_index);
- 		       // System.out.println(temp+"  B_index "+B_index+"  F_index "+F_index+"  W_index "+W_index);
+ 		         B_index+=Math.min(str.length(),target.length()-B_index); //Used min for borderline condition 
+ 
         	 	}
         		F_index++;
         		B_index++;
         	 }
-        //System.out.print("added"+target.substring(F_index-1,B_index-1));
-        ar.add(target.substring(F_index-1,B_index-1));
+ 
+        ar.add(target.substring(F_index-1,B_index-1));  //-1 added because incremating nature of while loop giving offset
         rtr=new String[ar.size()];    
-        rtr=ar.toArray(rtr);
+        rtr=ar.toArray(rtr); //converts arraylist to array
 		 return rtr;
 	}
 	
-	public String indexofallBrack(String str,String Target)
+	
+	
+	public String indexofallBrack(String str,String Target)  // Box cutter helper and replacer i.e [delimiter1][delimiter2] changes to a single delimter i.e ,
+	                                                        //which is returned to main function
 	{int index_FB=str.indexOf('[');
 	 int index_BB=str.indexOf(']');
 	// int cntr1=0,cntr0=0;
@@ -106,12 +103,5 @@ public int flag=0;
 		return T_str;
 	}
 	
-	
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-
-	}
 
 }
