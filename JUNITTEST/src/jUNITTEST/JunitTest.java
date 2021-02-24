8e98837ad9b2b2526996b88de39f78bdaabb2551
ParthB <41703901+ParthBasole@users.noreply.github.com>
2021-02-24 13:05:22 +0530

    Multiple Numbers Addition with Split by ","

--- JUNITTEST/src/jUNITTEST/JunitTest.java
+++ JUNITTEST/src/jUNITTEST/JunitTest.java
@@ -15,4 +15,13 @@ public class JunitTest {
 		public void testAdd() {
 	     assertEquals(0,calc.Add(""),"Null String should give out 0");
 }
+		
+		@Test
+		public void testAddS() {
+	     assertEquals(5,calc.Add("2,3"),"This String should give out 5");
+}
+		@Test
+		public void testAddMult() {
+	     assertEquals(24,calc.Add("2,3,6,7,6"),"This String should give out 5");
+}
 }
